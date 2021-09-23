c13e4603e5c837963c1c1eb80715914b1ee721be
Bryan Kuo <bryan0011@gmail.com>
2021-09-24 07:25:17 +0800

    Final

--- AutoLayout01/ViewController.swift
+++ AutoLayout01/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fullName: UITextField!
+    @IBOutlet weak var emailAddress: UITextField!
+    @IBOutlet weak var passWord: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        fullName.layer.borderWidth = 2
+        fullName.layer.borderColor = UIColor.black.cgColor
+        fullName.layer.cornerRadius = 10
+
+        emailAddress.layer.borderWidth = 2
+        emailAddress.layer.borderColor = UIColor.black.cgColor
+        emailAddress.layer.cornerRadius = 10
+        emailAddress.attributedPlaceholder = NSAttributedString(string: "    Email address", attributes: [NSAttributedString.Key.foregroundColor : UIColor.red])
+
+        passWord.layer.borderWidth = 2
+        passWord.layer.borderColor = UIColor.black.cgColor
+        passWord.layer.cornerRadius = 10
+        
     }
-
-
 }
 
